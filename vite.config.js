48a2c28123a5d0d139f37677e4ff9778191f1e28
AlinHuesca <angelicahuescagozalez@gmail.com>
2022-08-19 21:31:30 -0500

    FIX BUG

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://AlinHuesca.github.io/',
+  base: 'https://AlinHuesca.github.io/gh-react-vite/',
   plugins: [react()]
 })
